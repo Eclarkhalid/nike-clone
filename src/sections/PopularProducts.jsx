8f95fb49d21ba2ad7f19bb9c5f1b217fa710660f
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-20 12:02:56 +0300

    Updating the products gap

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -11,7 +11,7 @@ const PopularProducts = () => {
         </p>
       </div>
 
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 sm:gap-4 gap-4">
         {products.map((product) => (
           <PopularProductCard key={product.name} {...product} />
         ))}
