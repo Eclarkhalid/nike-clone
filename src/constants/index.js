c798aeca6297adf8191db34a0d77c85428eec5b3
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-20 11:55:31 +0300

    creating the popular products section

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,22 +33,22 @@ export const products = [
     {
         imgURL: shoe4,
         name: "Nike Air Jordan-01",
-        price: "$200.20",
+        price: "Kshs 20,000.20",
     },
     {
         imgURL: shoe5,
         name: "Nike Air Jordan-10",
-        price: "$210.20",
+        price: "Kshs 21,000.20",
     },
     {
         imgURL: shoe6,
         name: "Nike Air Jordan-100",
-        price: "$220.20",
+        price: "Kshs 22,000.20",
     },
     {
         imgURL: shoe7,
         name: "Nike Air Jordan-001",
-        price: "$230.20",
+        price: "Kshs 23,000.20",
     },
 ];
 
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import PopularProductCard from '../components/PopularProductCard'
+import { products } from '../constants/index'
 
 const PopularProducts = () => {
-  return (
-    <div>PopularProducts</div>
-  )
+  return <>
+    <div className="max-container max-sm:mt-12" id="products">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className='text-4xl font-palanquin'>Our Popular <span className="text-coral-red">Products</span></h2>
+        <p className=' lg:max-w-lg text-slate-gray mt-2 font-montserrat'>
+          Explore top-quality products meticulously crafted to elevate your athletic performance and enhance your personal style.
+        </p>
+      </div>
+
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </div>
+  </>
 }
 
 export default PopularProducts
