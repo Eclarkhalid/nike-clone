dc97e7c18d462f062eba37c6e8154a817c5454be
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-20 10:21:48 +0300

    creating Nike clone website

---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Services from './Services'
+import Footer from './Footer'
+import Subscribe from './Subscribe'
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+import SuperQuality from './SuperQuality'
+
+
+export {
+  Hero, SpecialOffer, Services, SuperQuality, Subscribe, CustomerReviews, Footer, PopularProducts
+}
